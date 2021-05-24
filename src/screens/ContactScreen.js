b552632c893f5c33b6a1277766e0510c5643c3d0
Pete Low <pedritolow@gmail.com>
2021-05-24 16:07:50 -0700

    added contact screen

---
+++ src/screens/ContactScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Contact from '../components/Contact'
+import EmailForm from '../components/EmailForm'
+
+const ContactScreen = () => {
+    return (
+        <>
+        <h1>Contact Point of Going</h1>
+          <Contact />
+           <EmailForm /> 
+        </>
+    )
+}
+
+export default ContactScreen
