e877e4d9f93c65912924c6c0a7e43862e52c1072
Pete Low <pedritolow@gmail.com>
2021-05-24 16:07:23 -0700

    added about secreen

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+
+
+const AboutScreen = () => {
+    return (
+        <>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia atque fugit pariatur natus praesentium fuga, cumque commodi dolorum rerum sunt vitae voluptatum placeat consectetur dolores, dolore maxime voluptates quia.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, mollitia atque fugit pariatur natus praesentium fuga, cumque commodi dolorum rerum sunt vitae voluptatum placeat consectetur dolores, dolore maxime voluptates quia.</p>
+            
+        </>
+    )
+}
+
+export default AboutScreen
